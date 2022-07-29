4cd2d1b45d1d3cbfa26d11a3eda5599d909faab0
Cory J Reid <cjrcodes@outlook.com>
2022-07-28 19:17:12 -0600

    Added dependencies for argument parsing & TOML handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.martiansoftware:jsap:2.1")
+    implementation("com.moandjiezana.toml:toml4j:0.7.2")
     implementation("de.erdbeerbaerlp:cfcore:1.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
