716cb95a85f99dd3e6fd39348407cd714702d4a3
Cory J Reid <cjrcodes@outlook.com>
2022-07-28 21:03:21 -0600

    Add argument parsing and 2 parameters

--- src/main/java/com/coryjreid/cursewiz/Cursewiz.java
+++ src/main/java/com/coryjreid/cursewiz/Cursewiz.java
@@ -1,7 +1,40 @@
 package com.coryjreid.cursewiz;
 
-public class Cursewiz {
-    public static void main(final String[] args) {
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
 
+public class Cursewiz {
+    private static final JSAP sJsap = new JSAP();
+
+    public static void main(final String[] args) throws JSAPException {
+        setupArgumentParser();
+
+        final JSAPResult parsedArgs = sJsap.parse(args);
+        if (!parsedArgs.success()) {
+            System.err.println();
+            System.err.println("Usage: java " + Cursewiz.class.getName() + " " + sJsap.getUsage());
+            System.err.println();
+            System.err.println(sJsap.getHelp());
+            System.err.println();
+            System.exit(1);
+        }
+    }
+
+    private static void setupArgumentParser() throws JSAPException {
+        sJsap.registerParameter(new FlaggedOption("curseforgeInstancePath")
+            .setStringParser(JSAP.STRING_PARSER)
+            .setRequired(true)
+            .setShortFlag('c')
+            .setLongFlag(JSAP.NO_LONGFLAG)
+            .setHelp("Path to the CurseForge App-managed Minecraft instance directory"));
+
+        sJsap.registerParameter(new FlaggedOption("modpackProjectPath")
+            .setStringParser(JSAP.STRING_PARSER)
+            .setRequired(true)
+            .setShortFlag('m')
+            .setLongFlag(JSAP.NO_LONGFLAG)
+            .setHelp("Path to the modpack project directory"));
     }
 }
